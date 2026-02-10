1eef88e5e71f6fa01578746de0ea9928971f9cb1
shun-mino <149372941+shun-mino@users.noreply.github.com>
2026-02-10 15:44:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -56,8 +56,7 @@ function calcPyr() {
   const B = 2*(x*sx + y*sy);
 
   document.getElementById("pr").textContent =
-    `表面積 ${A.toFixed(1)} mm² / ピッチ長さ ${pitch(A)} mm \n 
-     側面積 ${B.toFixed(1)} mm² / ピッチ長さ ${pitch(B)} mm`;
+    `表面積 ${A.toFixed(1)} mm² / ピッチ長さ ${pitch(A)} mm \n 側面積 ${B.toFixed(1)} mm² / ピッチ長さ ${pitch(B)} mm`;
 }
 
 /* Lアングル */
