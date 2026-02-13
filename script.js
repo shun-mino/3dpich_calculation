425e6213ac09a038766116b1137f49bd82c88bab
shun-mino <149372941+shun-mino@users.noreply.github.com>
2026-02-13 18:34:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function calcCylEllip() {
     `円柱側面積 ${Scyl.toFixed(1)} mm²<br>
      楕円下面積 ${Shalf.toFixed(1)} mm²<br>
      合計側面積 ${Stotal.toFixed(1)} mm²<br>
-     ピッチ長さ ${pitch(Stotal)} mm`　+
+     ピッチ長さ ${pitch(Stotal)} mm\n`　+
     `※表面積は空間に接しているすべての面の面積の合計である。\n` +
     `※側面積は底の面を除いた面の面積の合計である。`;
 }
