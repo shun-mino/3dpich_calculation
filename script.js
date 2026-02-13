4390c4094dcd8efe7eb97178effdc1db404fef00
shun-mino <149372941+shun-mino@users.noreply.github.com>
2026-02-13 14:48:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function showTab(id) {
 function pitch(area) {
   const L = Number(document.getElementById("heater").value);
   if (!L || L <= 0) return "ヒーター長さ未入力";
-  return (area / L) / 1000.toFixed(2);
+  return ((area / L) / 1000 ).toFixed(2);
 }
 
 /* 四角柱 */
